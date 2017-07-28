c84e94d7eedeb5ba116b27d01b3af2aa71873e4b
RoseLand96 <1716210001@utec-tgo.edu.mx>
2017-07-28 00:37:01 -0500

    Update turnos12.php

--- turnos12.php
+++ turnos12.php
@@ -1,23 +1,20 @@
 <?php
 
-$db_host = "rmspavs8mpub7dkq.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
-     $db_name = "n3ycfw3efefiynuy";
-     $db_user = "g1wbv4e6yvi6e710";
-     $db_password = "t2z30tum76qfgrzd";
-    
-     $connection = mysqli_connect($db_host, $db_user, $db_password) or die("Connection Error: " . mysql_error());
-    
-mysqli_select_db($db_name, $connection) or die("Error al seleccionar la base de datos:".mysql_error());
-    @mysqli_query("SET NAMES 'utf8'");
+define('HOST','rmspavs8mpub7dkq.chr7pe7iynqr.eu-west-1.rds.amazonaws.com');
+      	define('USER','g1wbv4e6yvi6e710');
+      	define('PASS','t2z30tum76qfgrzd');
+		define('DB','n3ycfw3efefiynuy');
+	 $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
+	
     $dia=strftime("%A");
 
 if(isset($_POST["id_servicio"])  && $dia=="Monday" || $dia=="Lunes"){
 
 $id_servicio = $_POST["id_servicio"];
 $sql_query = "SELECT * from turnos12_lunes where id_servicio=$id_servicio;";
-$result = mysqli_query($sql_query);
+$result = mysqli_query($con,$sql_query);
 $rows = array();
-while($r = mysql_fetch_assoc($result)) {
+while($r = mysqli_fetch_assoc($result)) {
   $rows[] = $r;
 }
 print json_encode($rows);
@@ -26,9 +23,9 @@ else if(isset($_POST["id_servicio"])  && $dia=="Tuesday" || $dia=="Martes"){
 
 $id_servicio = $_POST["id_servicio"];
 $sql_query = "SELECT * from turnos12_martes where id_servicio=$id_servicio;";
-$result =  mysqli_query($sql_query);
+$result = mysqli_query($con,$sql_query);
 $rows = array();
-while($r = mysql_fetch_assoc($result)) {
+while($r = mysqli_fetch_assoc($result)) {
   $rows[] = $r;
 }
 print json_encode($rows);
@@ -36,9 +33,9 @@ print json_encode($rows);
 
 $id_servicio = $_POST["id_servicio"];
 $sql_query = "SELECT * from turnos12_miercoles where id_servicio=$id_servicio;";
-$result =  mysqli_query($sql_query);
+$result = mysqli_query($con,$sql_query);
 $rows = array();
-while($r = mysql_fetch_assoc($result)) {
+while($r = mysqli_fetch_assoc($result)) {
   $rows[] = $r;
 }
 print json_encode($rows);
@@ -46,9 +43,9 @@ print json_encode($rows);
 
 $id_servicio = $_POST["id_servicio"];
 $sql_query = "SELECT * from turnos12_jueves where id_servicio=$id_servicio;";
-$result =  mysqli_query($sql_query);
+$result = mysqli_query($con,$sql_query);
 $rows = array();
-while($r = mysql_fetch_assoc($result)) {
+while($r = mysqli_fetch_assoc($result)) {
   $rows[] = $r;
 }
 print json_encode($rows);
@@ -57,9 +54,9 @@ else if(isset($_POST["id_servicio"])  && $dia=="Friday" || $dia=="Viernes"){
 
 $id_servicio = $_POST["id_servicio"];
 $sql_query = "SELECT * from turnos12_viernes where id_servicio=$id_servicio;";
-$result =  mysqli_query($sql_query);
+$result = mysqli_query($con,$sql_query);
 $rows = array();
-while($r = mysql_fetch_assoc($result)) {
+while($r = mysqli_fetch_assoc($result)) {
   $rows[] = $r;
 }
 print json_encode($rows);
@@ -67,9 +64,9 @@ print json_encode($rows);
 
 $id_servicio = $_POST["id_servicio"];
 $sql_query = "SELECT * from turnos12_sabado where id_servicio=$id_servicio;";
-$result = mysqli_query($sql_query);
+$result = mysqli_query($con,$sql_query);
 $rows = array();
-while($r = mysql_fetch_assoc($result)) {
+while($r = mysqli_fetch_assoc($result)) {
   $rows[] = $r;
 }
 print json_encode($rows);
@@ -78,9 +75,9 @@ else if(isset($_POST["id_servicio"])  && $dia=="Sunday" || $dia=="Domingo"){
 
 $id_servicio = $_POST["id_servicio"];
 $sql_query = "SELECT * from turnos12_domingo where id_servicio=$id_servicio;";
-$result = mysqli_query($sql_query);
+$result = mysqli_query($con,$sql_query);
 $rows = array();
-while($r = mysql_fetch_assoc($result)) {
+while($r = mysqli_fetch_assoc($result)) {
   $rows[] = $r;
 }
 print json_encode($rows);
@@ -88,6 +85,6 @@ print json_encode($rows);
 else
 {
 	echo "No existe el turno";
-mysqli_close($connection);
+mysqli_close($con);
 }
 ?>
